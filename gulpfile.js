94ffe2bd7d97fa56a7a84a952f834d7c67293250
jdollar <joel.dollarhide@passportinc.com>
2017-01-05 23:41:06 -0500

    Initial commit. Setting up workspace
    Started setting up the workspace. Setup a gulp file and starting setting up dependencies for typescript and react. Trying to get the general project structure filled out

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp')
+var fs = require('fs')
+var notifier = require('node-notifier')
+var browserify = require('browserify')
+var watchify = require('watchify')
+var tsify = require('tsify')
+
+var bundler = browserify({
+  entries: ['./src/ts/App.tsx'],
+  cache: {},
+  packageCache: {},
+  extensions: ['.jsx'],
+  plugin: [
+    watchify
+  ]
+}).plugin(tsify, { noImplicitAny: true })
+
+function notify(error) {
+  console.log(error.message)
+  var fileName = error.message.match(/[^/]+$/)
+  notifier.notify({title: fileName, message: error.description})
+}
+
+function bundle() {
+  bundler.bundle()
+    .on('error', notify)
+    .pipe(fs.createWriteStream('./build/myanimelist.js'))
+}
+
+function copyContentToBuild() {
+  try{
+    fs.statSync('build')
+  } catch(err) {
+    fs.mkdirSync('build')
+  }
+
+  //gulp.src(['node_modules/semantic-ui-css/semantic.min.css', 'src/**/*', '!src/js/{,**}']).pipe(gulp.dest('build'))
+}
+
+gulp.task('bundle', function() {
+  bundle()
+})
+
+gulp.task('build', function() {
+  copyContentToBuild()
+})
+
+gulp.task('deployBuild', ['build', 'bundle'])
+gulp.task('dev', ['build', 'bundle'], function() {
+  bundler.on('update', bundle)
+})
+gulp.task('default', ['dev'])
